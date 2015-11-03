f7174b3ee67008ef99c37ba407815250c5dbc42f
karvel <jadeilyn@yahoo.com>
2015-11-02 20:58:40 -0800

    Initial commit.

---
+++ app01.js
@@ -0,0 +1,13 @@
+/*
+Elanna Grossman
+November 2, 2015
+GeekWise Javascript I
+*/
+
+var var1 = 12;
+var var2 = 0x1e;
+var var3 = "Answer equals = ";
+var var4 = var1 + var2;
+
+var answer = var3 + var4;
+answer
